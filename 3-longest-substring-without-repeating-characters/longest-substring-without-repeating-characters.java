37480e5622efdb682aa4258ffa5108db921733e2
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-07-08 20:00:27 +0530

    Time: 6 ms (72.18%) | Memory: 44.8 MB (56.83%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,14 +3,14 @@ class Solution {
         HashSet<Character> set=new HashSet<>();
         int m=0,l=0,r=0;
         while(r<s.length()){
-            while(r<s.length()&&!set.contains(s.charAt(r))){
-                set.add(s.charAt(r));
-                r++;
-            }
-            m=m>(r-l)?m:(r-l);
+           while(set.contains(s.charAt(r))){
+            set.remove(s.charAt(l));
             l++;
-            r=l;
-            set.clear();
+           }
+           set.add(s.charAt(r));
+            m=m>(r-l+1)?m:(r-l+1);
+            r++;
+            
             
         }
         return m;
