aa44b23a0dc7044a4f0eb65c67eaee817985a1c4
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-07-15 21:15:43 +0530

    Time: 11 ms (68.78%) | Memory: 44.6 MB (91.57%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -4,15 +4,12 @@ class Solution {
         if (s.length() < p.length()) return arr;
         int freq1[] = new int[26];
         int freq2[] = new int[26];
-
         for (char c : p.toCharArray()) {
             freq1[c - 'a']++;
         }
-
         for (int i = 0; i < p.length(); i++) {
             freq2[s.charAt(i) - 'a']++;
         }
-
         if (Arrays.equals(freq1, freq2)) {
             arr.add(0);
         }
@@ -20,13 +17,11 @@ class Solution {
         for (int i = 1; i <=s.length()-p.length(); i++) {
             freq2[s.charAt(l) - 'a']--;
             freq2[s.charAt(l+p.length()) - 'a']++;
-
             if (Arrays.equals(freq1, freq2)) {
                 arr.add(i);
             }
             l++;
         }
-
-        return arr;
+       return arr;
     }
 }
