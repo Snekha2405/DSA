03ba424c3cf2310689cb21b9affa59746e57bc9c
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-07-24 19:39:03 +0530

    Time: 343 ms (19.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,9 @@
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM requestaccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM requestaccepted
+) AS all_friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
