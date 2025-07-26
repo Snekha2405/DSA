f211413b2155cb6426bda8bca80b285345f207a4
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-07-26 13:26:47 +0530

    Time: 326 ms (79.00%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -3,7 +3,6 @@ SELECT
     WHEN id % 2 = 1 AND id + 1 <= (SELECT MAX(id) FROM Seat) THEN id + 1
     WHEN id % 2 = 0 THEN id - 1
     ELSE id
-  END AS id,
-  student
+  END AS id,student
 FROM Seat
 ORDER BY id;
