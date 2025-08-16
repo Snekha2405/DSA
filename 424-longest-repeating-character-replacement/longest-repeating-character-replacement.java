604f0a2a05b7999c51266f1484aaf92b451451d1
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-08-16 13:58:15 +0530

    Time: 10 ms (67.24%) | Memory: 45.1 MB (40.27%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int freq[]=new int[26];
+        int maxLen=0,maxCount=0,left=0;
+        for(int right=0;right<s.length();right++)    // pa
+        {
+            int index=s.charAt(right)-'A';
+            freq[index]++;
+            // System.out.println("Right:"+right);
+            // System.out.println("FreqCount:"+s.charAt(right)+"-"+freq[index]);
+            maxCount=Math.max(maxCount,freq[index]);
+            // System.out.println("MaxCount:"+maxCount);
+            // System.out.println("right-left+1-maxCount:"+(right-left+1-maxCount));
+            if(right-left+1-maxCount>k)
+            {
+            // System.out.println("Ulla poiduchu right-left+1-maxCount "+(right-left+1-maxCount+"irukamodhu"));
+                freq[s.charAt(left)-'A']--;
+            // System.out.println("freq[s.charAt(left)-'A']-- pana aprm "+freq[s.charAt(left)-'A']);
+                left++;
+            // System.out.println("Left:"+left);
+            }
+            maxLen=Math.max(maxLen,right-left+1);
+            // System.out.println("maxLen:"+maxLen);
+        }
+        return maxLen;
+    }
+}
