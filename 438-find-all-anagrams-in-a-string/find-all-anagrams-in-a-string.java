9bce69237ee83adf833a2e5a3a6af6230d923d7c
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-08-16 16:42:50 +0530

    Time: 10 ms (80.21%) | Memory: 45 MB (58.43%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> arr = new ArrayList<>();
-        if (s.length() < p.length()) return arr;
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
-        for (char c : p.toCharArray()) {
-            freq1[c - 'a']++;
-        }
-        for (int i = 0; i < p.length(); i++) {
-            freq2[s.charAt(i) - 'a']++;
-        }
-        if (Arrays.equals(freq1, freq2)) {
-            arr.add(0);
-        }
-        int l=0;
-        for (int i = 1; i <=s.length()-p.length(); i++) {
-            freq2[s.charAt(l) - 'a']--;
-            freq2[s.charAt(l+p.length()) - 'a']++;
-            if (Arrays.equals(freq1, freq2)) {
-                arr.add(i);
-            }
-            l++;
-        }
-       return arr;
+    if(s.length()<p.length()) return new ArrayList<>();
+    int freq1[]=new int[26];
+    for(char c:p.toCharArray())
+        freq1[c-'a']++;
+    int freq2[]=new int[26];
+    for(int i=0;i<p.length();i++)
+        freq2[s.charAt(i)-'a']++;
+    List<Integer> ans=new ArrayList<>();
+    if(Arrays.equals(freq1,freq2)) ans.add(0);
+    int left=0;
+    for(int right=p.length();right<s.length();right++){
+        freq2[s.charAt(right)-'a']++;
+        freq2[s.charAt(left)-'a']--;
+        left++;
+        if(Arrays.equals(freq1,freq2)) ans.add(left);
+    }
+    return ans;
+        
+            
+
+
+        
     }
 }
