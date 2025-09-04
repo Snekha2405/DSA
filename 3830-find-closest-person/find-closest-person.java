c96fbbcee7059ec2c2db6b9bb02bd05d3b816fe9
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-09-04 17:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (95.25%) - LeetSync

--- 3830-find-closest-person/find-closest-person.java
+++ 3830-find-closest-person/find-closest-person.java
@@ -2,8 +2,8 @@ class Solution {
     public int findClosest(int x, int y, int z) {
         int dis1=Math.abs(x-z);
         int dis2=Math.abs(y-z);
-        if(dis1<dis2) return 1;
         if(dis1==dis2) return 0;
+        if(dis1<dis2) return 1;
         return 2;
         
     }
