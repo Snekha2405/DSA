504f1103beb1cad61f518091746bbd379fa8fd13
SNEKHA R <125180989+Snekha2405@users.noreply.github.com>
2025-09-06 18:37:06 +0530

    Time: 14 ms (7.65%) | Memory: 48 MB (6.53%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -1,28 +1,36 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int n=mat.length,m=mat[0].length;
-        int res[]=new int[n*m];
-        int row=0,col=0;
-        for(int i=0;i<n*m;i++){
-            res[i]=mat[row][col];
-            if((row+col)%2==0){
-            if(row>0 && col<m-1){
-                row--;
-                col++;
+        int n=mat.length;
+        int m=mat[0].length;
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(map.containsKey(i+j))
+                  map.get(i+j).add(mat[i][j]);
+                else{
+                    List<Integer> arr=new ArrayList<>();
+                    arr.add(mat[i][j]);
+                    map.put(i+j,arr);
+                }
             }
-            else if(row==0 && col<m-1) col++;
-            else row++;  
+
         }
-        else{
-            if(row<n-1 && col>0){
-                row++;
-                col--;
-            } 
-            else if(col==0 && row<n-1) row++;   
-            else col++;   
+         List<Integer> resultList = new ArrayList<>();
+       for (int k = 0; k <= n + m - 2; k++) {
+            List<Integer> diag = map.get(k);
+            if (k % 2 == 0) {  
+                // Reverse order for even diagonals
+                Collections.reverse(diag);
+            }
+            resultList.addAll(diag);
         }
+
+        // Convert list to array
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = resultList.get(i);
         }
-        return res;
+        return result;
 
         
     }
